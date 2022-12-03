a4be356f9325e7bc19ddf71b0509324813c1b549
Divyam <agrawaldivyam2001@gmail.com>
2022-12-04 01:00:12 +0530

    Minor updates

--- src/layouts/Sidebar/Menu/Menu.js
+++ src/layouts/Sidebar/Menu/Menu.js
@@ -6,17 +6,22 @@ import { config } from "~/config";
 import WrapperAuth from "~/components/WrapperAuth";
 import Tab from "./Tab";
 
+import { useWalletProvider } from "~/context/WalletProvider"
+
 function Menu() {
+  const { isLoggedIn } = useWalletProvider();
+
   return (
     <nav className={styles.sidebar_tabs}>
       <Tab to={config.routes.home} Icon={FaHome}>
         For you
       </Tab>
-      <WrapperAuth>
+      {
+        isLoggedIn &&
         <Tab to={config.routes.following} Icon={BsPeople}>
           Following
         </Tab>
-      </WrapperAuth>
+      }
       <Tab to={config.routes.live} Icon={BsCameraVideo}>
         Live
       </Tab>
