e348c83dd154ab6afd6109893de0ed379b1bafe7
Divyam <agrawaldivyam2001@gmail.com>
2022-12-04 05:47:56 +0530

    Minor name changes

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -168,7 +168,7 @@ function Upload() {
         },
       ],
       animation_url: videoURL,
-      appId: "tiktok",
+      appId: "lenstokV2",
     };
 
     const blob = new Blob([JSON.stringify(pubMetadata)], { type: 'application/json' })
